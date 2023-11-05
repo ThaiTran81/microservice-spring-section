5fa32cfb7df69a0b9fd8288194dbadf63b918690
Thai Tran <tranhoangthai2001@gmail.com>
2023-11-05 22:12:03 +0700

    day 01: warm up with spring basic

---
+++ day01/accounts/src/main/java/com/thaitran81/accounts/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.thaitran81.accounts.repository;
+
+import com.thaitran81.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+
+
+}
